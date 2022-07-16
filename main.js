0d4c759e34d98d16378b04960c2add18674682fc
Yarden Edry <yardenedry0@gmail.com>
2022-07-16 12:59:39 -0400

    noted final

--- main.js
+++ main.js
@@ -6,9 +6,22 @@ function createElement(parent, content, type) {
   return newElement;
 }
 
+chrome.runtime.getBackgroundPage(function (bg) {
+  if (bg.sessionDataHTML) {
+    document.body.innerHTML = bg.sessionDataHTML;
+  }
+  setInterval(function () {
+    bg.sessionDataHTML = document.body.innerHTML
+  }, 1000);
+
+  //do the rest of your work here.
+
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   let display = document.getElementById("display");
-  let saveButton = document.getElementById("save-button");
+  let saveButton = document.getElementById("save-btn");
+  let userInput = document.getElementById("user-input");
 
   // accesses message from background.js
   chrome.storage.sync.get("message", ({ message }) => {
@@ -19,11 +32,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // testing addEventListener on clicking the save button within extension
   saveButton.addEventListener("click", () => {
     console.log("hello, does this work");
-    const highlight = prompt(); // user input
+    const highlight = userInput.value; // user input
     console.log(highlight); // check on console
     const highlightEl = createElement(display, highlight, "div"); // append to display
     highlightEl.setAttribute("class", "messages");
+    userInput.value = "";
+
+    // let key = (Math.random() + 1).toString(36).substring(7);
+    // chrome.storage.local.set({ key: highlight }, function () {
+    //   console.log('Value is set to ' + highlight);
+    // });
+
+    // chrome.storage.local.get(['key'], function (result) {
+    //   console.log('Value currently is ' + result.key);
+    // });
   });
+
+
 
   // onclick save button, invoke saveAndUpdate 
   // saveButton.addEventListener("click", async () => {
@@ -38,21 +63,21 @@ document.addEventListener("DOMContentLoaded", function () {
   function getHighlight() {
     let txt = "";
     if (window.getSelection) {
-        txt = window.getSelection().toString();
+      txt = window.getSelection().toString();
     } else if (document.getSelection) {
-        txt = document.getSelection();
+      txt = document.getSelection();
     } else if (document.selection) {
-        txt = document.selection.createRange().text;
+      txt = document.selection.createRange().text;
     }
     return txt;
   };
-    
-  // function - save highlighted or selected text to object and update display with 
+
+  // function - save highlighted or selected text to object and update display with
   function saveAndUpdate() {
     console.log("inside saveAndUpdate func");
 
     // store highlighted text in variable
-    
+
     // add highlighted text to object with key as date object + value as text
 
     // update object output on display by invoking displayHighlights()
